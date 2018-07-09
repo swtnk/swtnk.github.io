b4dd1200b51946d5bf25e7fdc113362c13f95ee8
Swetank Subham <swetanksubham.r@gmail.com>
2018-07-09 20:31:12 +0530

    1.0.2

--- static/javascript/custom.js
+++ static/javascript/custom.js
@@ -1,34 +1,3 @@
 $(function(){
-    // Codes Here
     $('.sidenav').sidenav();
-    $('.degree-desc').css({
-        'height' : $('.degree-container').height()
-    });
-    if($(window).width() <= 600){
-        $('.degree-desc').css({
-            'width' : '100%'
-        });
-        $('.degree-container').css({
-            'margin-top' : '-14px'
-        });
-        $('.degree-desc').css({
-            'height' : 'auto'
-        });
-    };
-    $(window).resize(() => {
-        $('.degree-desc').css({
-            'height' : $('.degree-container').height()
-        });
-        if($(window).width() <= 600){
-            $('.degree-desc').css({
-                'width' : '100%'
-            });
-            $('.degree-container').css({
-                'margin-top' : '-14px'
-            });
-            $('.degree-desc').css({
-                'height' : 'auto'
-            });
-        };
-    });
 })
