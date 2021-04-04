e0ae8082b058b55ad28f4b78d48d638884d395cd
swtnk <swetanksubham.r@gmail.com>
2021-04-04 10:13:19 +0530

    education updated

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -15,95 +15,95 @@
   \*************************/
 /***/ (() => {
 
-eval("window.onload = function () {\n  var themeSwitch = document.querySelector('#theme-switch'),\n      htmlElement = document.querySelector('html');\n\n  if (themeSwitch) {\n    themeSwitch.addEventListener('click', function (event) {\n      currentTheme = htmlElement.getAttribute('theme');\n\n      if (currentTheme === 'light') {\n        htmlElement.setAttribute('theme', 'dark');\n      } else {\n        htmlElement.setAttribute('theme', 'light');\n      }\n    });\n  }\n};\n\n//# sourceURL=webpack://WebProfiler/./src/js/index.js?");
+  eval("function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }\n\nfunction _nonIterableRest() { throw new TypeError(\"Invalid attempt to destructure non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\"); }\n\nfunction _iterableToArrayLimit(arr, i) { if (typeof Symbol === \"undefined\" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i[\"return\"] != null) _i[\"return\"](); } finally { if (_d) throw _e; } } return _arr; }\n\nfunction _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }\n\nfunction _createForOfIteratorHelper(o, allowArrayLike) { var it; if (typeof Symbol === \"undefined\" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === \"number\") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e2) { throw _e2; }, f: F }; } throw new TypeError(\"Invalid attempt to iterate non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\"); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e3) { didErr = true; err = _e3; }, f: function f() { try { if (!normalCompletion && it[\"return\"] != null) it[\"return\"](); } finally { if (didErr) throw err; } } }; }\n\nfunction _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === \"string\") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === \"Object\" && o.constructor) n = o.constructor.name; if (n === \"Map\" || n === \"Set\") return Array.from(o); if (n === \"Arguments\" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }\n\nfunction _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }\n\nwindow.onload = function () {\n  var themeSwitch = document.querySelector('#theme-switch'),\n      htmlElement = document.querySelector('html'),\n      infoSection = document.querySelectorAll('.info-section');\n\n  if (themeSwitch) {\n    themeSwitch.addEventListener('click', function (event) {\n      currentTheme = htmlElement.getAttribute('theme');\n\n      if (currentTheme === 'light') {\n        htmlElement.setAttribute('theme', 'dark');\n      } else {\n        htmlElement.setAttribute('theme', 'light');\n      }\n    });\n  }\n\n  if (infoSection) {\n    var _iterator = _createForOfIteratorHelper(infoSection.entries()),\n        _step;\n\n    try {\n      for (_iterator.s(); !(_step = _iterator.n()).done;) {\n        var _step$value = _slicedToArray(_step.value, 2),\n            index = _step$value[0],\n            element = _step$value[1];\n\n        element.addEventListener('scroll', function (event) {\n          event.preventDefault();\n          console.log('event');\n        });\n      }\n    } catch (err) {\n      _iterator.e(err);\n    } finally {\n      _iterator.f();\n    }\n  }\n};\n\n//# sourceURL=webpack://WebProfiler/./src/js/index.js?");
 
-/***/ }),
-
-/***/ "./src/css/main.scss":
-/*!***************************!*\
-  !*** ./src/css/main.scss ***!
-  \***************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"css/main.css\");\n\n//# sourceURL=webpack://WebProfiler/./src/css/main.scss?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl + "../";
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	__webpack_modules__["./src/js/index.js"](0, {}, __webpack_require__);
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/css/main.scss"](0, __webpack_exports__, __webpack_require__);
-/******/ 	
-/******/ })()
-;
+  /***/ }),
+  
+  /***/ "./src/css/main.scss":
+  /*!***************************!*\
+    !*** ./src/css/main.scss ***!
+    \***************************/
+  /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+  
+  "use strict";
+  eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (__webpack_require__.p + \"css/main.css\");\n\n//# sourceURL=webpack://WebProfiler/./src/css/main.scss?");
+  
+  /***/ })
+  
+  /******/ 	});
+  /************************************************************************/
+  /******/ 	// The require scope
+  /******/ 	var __webpack_require__ = {};
+  /******/ 	
+  /************************************************************************/
+  /******/ 	/* webpack/runtime/define property getters */
+  /******/ 	(() => {
+  /******/ 		// define getter functions for harmony exports
+  /******/ 		__webpack_require__.d = (exports, definition) => {
+  /******/ 			for(var key in definition) {
+  /******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+  /******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+  /******/ 				}
+  /******/ 			}
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/global */
+  /******/ 	(() => {
+  /******/ 		__webpack_require__.g = (function() {
+  /******/ 			if (typeof globalThis === 'object') return globalThis;
+  /******/ 			try {
+  /******/ 				return this || new Function('return this')();
+  /******/ 			} catch (e) {
+  /******/ 				if (typeof window === 'object') return window;
+  /******/ 			}
+  /******/ 		})();
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+  /******/ 	(() => {
+  /******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/make namespace object */
+  /******/ 	(() => {
+  /******/ 		// define __esModule on exports
+  /******/ 		__webpack_require__.r = (exports) => {
+  /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+  /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+  /******/ 			}
+  /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+  /******/ 		};
+  /******/ 	})();
+  /******/ 	
+  /******/ 	/* webpack/runtime/publicPath */
+  /******/ 	(() => {
+  /******/ 		var scriptUrl;
+  /******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+  /******/ 		var document = __webpack_require__.g.document;
+  /******/ 		if (!scriptUrl && document) {
+  /******/ 			if (document.currentScript)
+  /******/ 				scriptUrl = document.currentScript.src
+  /******/ 			if (!scriptUrl) {
+  /******/ 				var scripts = document.getElementsByTagName("script");
+  /******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+  /******/ 			}
+  /******/ 		}
+  /******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+  /******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+  /******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+  /******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+  /******/ 		__webpack_require__.p = scriptUrl + "../";
+  /******/ 	})();
+  /******/ 	
+  /************************************************************************/
+  /******/ 	
+  /******/ 	// startup
+  /******/ 	// Load entry module and return exports
+  /******/ 	// This entry module can't be inlined because the eval devtool is used.
+  /******/ 	__webpack_modules__["./src/js/index.js"](0, {}, __webpack_require__);
+  /******/ 	var __webpack_exports__ = {};
+  /******/ 	__webpack_modules__["./src/css/main.scss"](0, __webpack_exports__, __webpack_require__);
+  /******/ 	
+  /******/ })()
+  ;
