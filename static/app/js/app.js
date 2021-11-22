f3ff6913a590cdeae6d68643efd900515f0ec82d
Swetank Subham <swetank.subham@knowlarity.com>
2021-11-22 19:53:30 +0530

    style changes

--- static/app/js/app.js
+++ static/app/js/app.js
@@ -1,5 +1,18 @@
 import typewriterEffect from './helper.js';
+import resume_builder from './resume.js';
 (() => {
     const dataText = ['Software Developer', 'Team Player', 'Learner', 'Data Science Enthusiast'];
     typewriterEffect('#typewriter', dataText, 100, 600, 10000);
+
+    const build_resume = (element) => {
+        fetch('./static/app/data/resume.json').then(
+            response => response.json()
+        ).then(data => {
+            resume_builder(data, element);
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    build_resume('#resume');
 })()
